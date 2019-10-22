54d90d7a4157b63eaa1a12c6372b102ec2aeaf99
wangzefeng <wangzefeng@gdcattsoft.com>
2019-10-22 10:27:07 +0800

    problem876 solution

---
+++ src/leetcode/Problem876.java
@@ -0,0 +1,72 @@
+package leetcode;
+
+/**
+ * <p>
+ * 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+ * <p>
+ * 如果有两个中间结点，则返回第二个中间结点。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：[1,2,3,4,5]
+ * 输出：此列表中的结点 3 (序列化形式：[3,4,5])
+ * 返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+ * 注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+ * 示例 2：
+ * <p>
+ * 输入：[1,2,3,4,5,6]
+ * 输出：此列表中的结点 4 (序列化形式：[4,5,6])
+ * 由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+ * <p>
+ * 提示：
+ * <p>
+ * 给定链表的结点数介于 1 和 100 之间。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/middle-of-the-linked-list
+ * </p>
+ *
+ * @author wangzefeng
+ * @since JDK1.8
+ */
+public class Problem876 {
+
+    public static void main(String[] args) {
+
+    }
+
+    // 快慢指针法
+    private static ListNode middleNode(ListNode head) {
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+
+    // 数组取中间索引值 666
+    private ListNode middleNode2(ListNode head) {
+        ListNode[] A = new ListNode[100];
+        int t = 0;
+        while (head.next != null) {
+            A[t++] = head;
+            head = head.next;
+        }
+        return A[t / 2];
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
